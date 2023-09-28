c8b9a01a88dd977bb1e87426e7a89d19088dad9c
Nguyen Duy Khanh <nguyenkhanh17112003@gmail.com>
2023-09-28 17:01:29 +0700

    anotation

--- THUC_HANH_1/Controllers/StudentController.cs
+++ THUC_HANH_1/Controllers/StudentController.cs
@@ -20,19 +20,19 @@ namespace THUC_HANH_1.Controllers
             {
                 new Student() { Id = 101, Name = "Hải Nam", Branch = Branch.IT,
                 Gender = Gender.Male, IsRegular=true,
-                Address = "A1-2018", Email = "nam@g.com",Score = 8 },
+                Address = "A1-2018", Email = "nam@g.com" },
 
                 new Student() { Id = 102, Name = "Minh Tú", Branch = Branch.BE,
                 Gender = Gender.Female, IsRegular=true,
-                Address = "A1-2019", Email = "tu@g.com", Score = 9 },
+                Address = "A1-2019", Email = "tu@g.com" },
 
                 new Student() { Id = 103, Name = "Hoàng Phong", Branch = Branch.CE,
                 Gender = Gender.Male, IsRegular=false,
-                Address = "A1-2020", Email = "phong@g.com" , Score = 10 },
+                Address = "A1-2020", Email = "phong@g.com" },
 
                 new Student() { Id = 104, Name = "Xuân Mai", Branch = Branch.EE,
                 Gender = Gender.Female, IsRegular = false,
-                Address = "A1-2021", Email = "mai@g.com" , Score = 9.5
+                Address = "A1-2021", Email = "mai@g.com"
                 
                }
 
@@ -50,12 +50,7 @@ namespace THUC_HANH_1.Controllers
         [HttpGet("Add")]
         public IActionResult Create(Student s)
         {
-            if (ModelState.IsValid)
-            {
-                s.Id = listStudents.Last<Student>().Id + 1;
-                listStudents.Add(s);
-                return View("Index", listStudents);
-            }
+            
             //lấy danh sách các giá trị Gender để hiển thị radio button trên form
             ViewBag.AllGenders = Enum.GetValues(typeof(Gender)).Cast<Gender>().ToList();
             //lấy danh sách các giá trị Branch để hiển thị select-option trên form
@@ -74,12 +69,6 @@ namespace THUC_HANH_1.Controllers
         [HttpPost("Add")]
         public async Task<IActionResult> Create(Student s, IFormFile avatarfile)
         {
-            if (ModelState.IsValid)
-            {
-                s.Id = listStudents.Last<Student>().Id + 1;
-                listStudents.Add(s);
-                return View("Index", listStudents);
-            }
             if (avatarfile != null)
                 try
                 {
@@ -100,15 +89,28 @@ namespace THUC_HANH_1.Controllers
                     //Log ex
                     ViewBag.Message = "File Upload Failed";
                 }
-
-            s.Id = listStudents.Last<Student>().Id + 1;
-
-            // Handle Avatar Url. 
-            if (s.AvatarUrl != null)
-                s.AvatarUrl = Path.Combine("UploadedFiles", s.AvatarUrl);
-
-            listStudents.Add(s);
-            return View("Index", listStudents);
+            if (ModelState.Root.Children != null && ModelState.Root.Children.Count > 8)
+            {
+                ModelState.Root.Children[9].ValidationState = Microsoft.AspNetCore.Mvc.ModelBinding.ModelValidationState.Valid;
+            }
+            //
+            // Handling store The new student
+            //
+            if (ModelState.IsValid)
+            {
+                s.Id = listStudents.Last<Student>().Id + 1;
+                listStudents.Add(s);
+                return View("Index", listStudents);
+            }
+            ViewBag.AllGenders = Enum.GetValues(typeof(Gender)).Cast<Gender>().ToList();
+            ViewBag.AllBranches = new List<SelectListItem>()
+            {
+                new SelectListItem { Text = "IT", Value = "1" },
+                new SelectListItem { Text = "BE", Value = "2" },
+                new SelectListItem { Text = "CE", Value = "3" },
+                new SelectListItem { Text = "EE", Value = "4" }
+            };
+            return View();
         }
     }
 }
--- THUC_HANH_1/Models/Student.cs
+++ THUC_HANH_1/Models/Student.cs
@@ -1,38 +1,61 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
 using THUC_HANH_1.Models;
 
 namespace THUC_HANH_1.Models
 {
     public class Student
     {
-        public int Id { get; set; } // Ma sinh vien
-       
-        [StringLength(100, MinimumLength = 4)]
-        [Required(ErrorMessage = "Name tối thiểu 4 ký tự, tối đa 100 ký tự")]
-        public string? Name { get; set; } // Ho ten
-        [Required(ErrorMessage = "Địa chỉ email phải có đuôi gmail.com")]
-        [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}")]
-        public string? Email { get; set; }
-        [StringLength(100, MinimumLength = 8)]
-        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&+=!]).{8,}$")]
-        [Required(ErrorMessage = "Mật khẩu từ 8 ký tự trở lên, có ký tự viết hoa, viết thường, chữ số và ký tự đặc biệt")]
-        public string? Password { get; set; }
-        [Required(ErrorMessage = "Vui lòng nhập trường này")]
-        public Branch? Branch { get; set; }
-        [Required(ErrorMessage = "Vui lòng nhập trường này")]
-        public Gender? Gender { get; set; }
-        public bool IsRegular { get; set; }
-        [DataType(DataType.MultilineText)]
-        [Required(ErrorMessage = "Vui lòng nhập trường này")]
-        public string? Address { get; set; }
-        [Range(typeof(DateTime), "1/1/1963", "12/31/2005")]
-        [DataType(DataType.Date)]
-        [Required(ErrorMessage = "Vui lòng nhập trường này ")]
-        public DateTime DateOfBorth { get; set; }
-        public string? AvatarUrl { get; set; }
+        public int Id { get; set; } //Mã sinh viên
 
-        [Required(ErrorMessage = "Điểm phải nằm trong khoảng từ 0.0 đến 10.0")]
-        [Range(0.0, 10.0, ErrorMessage = "Điểm phải nằm trong khoảng từ 0.0 đến 10.0")]
-        public double Score { get; set; }
+        [Required(ErrorMessage = "Họ tên bắt buộc phải được nhập. Tối thiểu 4 ký tự, tối đa 100 ký tự.")]
+        [Display(Name = "Họ tên")]
+        [StringLength(100, MinimumLength = 4, ErrorMessage = "Họ tên tối thiểu 4 ký tự, tối đa 100 ký tự.")]
+        public string? Name { get; set; } //Họ tên
+
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "Email phải được nhập.")]
+        [RegularExpression(@"^[^@\s]+@gmail\.com$", ErrorMessage = "Email không hợp lệ.")]
+        public string? Email { get; set; } //Email
+
+        [Display(Name = "Mật khẩu")]
+        [StringLength(100, MinimumLength = 8)]
+        [Required(ErrorMessage = "Mật khẩu bắt buộc phải được nhập.")]
+        [RegularExpression(@"(?=.*[0-9])(?=.*[A-Z])(?=.*[!@#%^&*()_+\-=\[\]{};':""\\|,.<>\/?~]).{8,}",
+            ErrorMessage = "Mật khẩu từ 8 ký tự trở lên, có ký tự viết hoa, viết thường, chữ số và ký tự đặc biệt")]
+        public string? Password { get; set; } //Mật khẩu
+
+        [Display(Name = "Ngành")]
+        [Required(ErrorMessage = "Chưa chọn ngành học")]
+        public Branch? Branch { get; set; } //Ngành học
+
+        [Display(Name = "Giới tính")]
+        [Required(ErrorMessage = "Chưa chọn giới tính")]
+        public Gender? Gender { get; set; } //Giới tính
+
+        [Display(Name = "Chính quy")]
+        public bool IsRegular { get; set; } //Hệ: true-chính quy, false-phi chính quy
+
+        [Display(Name = "Địa chỉ")]
+        [DataType(DataType.MultilineText)]
+        public string? Address { get; set; } //Địa chỉ
+
+        [Display(Name = "Ngày sinh")]
+        [Range(typeof(DateTime), "01/01/1963 00:00", "01/01/2006 00:00",
+            ErrorMessage = "Ngày sinh phải từ 01/01/1963 - 31/12/2005")]
+
+        [DataType(DataType.Date)]
+        [Required(ErrorMessage = "Chưa chọn ngày sinh.")]
+        public DateTime DateOfBorth { get; set; } //Ngày sinh
+
+        [Display(Name = "Avatar")]
+        [AllowNull]
+        public string? AvatarUrl { get; set; } // Ảnh đại diện sinh viên
+
+        [DataType(DataType.Currency)]
+        //[Required(ErrorMessage = "Bắt buộc nhập điểm.")]
+        [Display(Name = "Điểm")]
+        [Range(0, 10.0, ErrorMessage = "Điểm phải từ 0.0 - 10.0")]
+        public float? Mark { get; set; }
     }
 }
